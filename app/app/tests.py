3f0ab986286dfa1176ce1df4cfd635b635d8f62f
rachel <rachel@gmail.com>
2024-04-27 15:22:59 -0700

    fixed linting

--- app/app/tests.py
+++ app/app/tests.py
@@ -7,15 +7,15 @@ from app import calc
 
 
 class CalcTests(SimpleTestCase):
-    
+  
     def test_add_numbers(self):
 
         res = calc.add(3, 8)
 
         self.assertEqual(res, 11)
-    
+
     def test_subtract_numbers(self):
 
-        res = calc.subtract(5,11)
+        res = calc.subtract(5, 11)
 
         self.assertEqual(res, -6)
